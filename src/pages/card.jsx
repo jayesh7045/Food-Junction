aa51556712959c500ca960b35bd360ef0b79e7a9
jayesh7045 <jayeshwadhwani108@gmail.com>
2024-09-24 23:32:49 +0530

    resolve build errors

--- src/pages/card.jsx
+++ src/pages/card.jsx
@@ -1,5 +1,5 @@
-import Card from "../components/card"
-export default function Card(){
+import Card from "../components/Card"
+export default function card(){
     return(
         <>
             <Card/>
